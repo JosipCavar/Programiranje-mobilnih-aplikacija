7f1552ba392d0e2f597fcc3db2086e5f11b6100b
Josip Ćavar <josipcavar10@gmail.com>
2026-05-11 11:08:16 +0200

    Leaderboar i hardware dopustanje

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout() {
       <AuthProvider>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="game-one" options={{ title: "Game One" }} />
           <Stack.Screen
             name="modal"
             options={{ presentation: "modal", title: "Modal" }}
